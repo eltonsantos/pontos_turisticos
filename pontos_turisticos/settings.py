0be575ce2e044b1a1d06e52e18a5ea02fab0dcb2
Elton Santos <eltonax@hotmail.com>
2020-06-26 02:10:53 -0300

    Configuring the app

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -27,7 +27,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://pontos-turisticos-django-djf.herokuapp.com', 'localhost:8000']
+ALLOWED_HOSTS = ['pontos-turisticos-django-drf.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
